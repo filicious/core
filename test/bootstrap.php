0cea56a0420d11181762a21cbb5468467085e2af
Tristan Lins <tristan.lins@infinitysoft.de>
2012-10-17 17:15:37 +0200

    Initial test import.

---
+++ test/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once(__DIR__ . '/../src/bit3/filesystem/File.php');
+require_once(__DIR__ . '/../src/bit3/filesystem/Filesystem.php');
+require_once(__DIR__ . '/../src/bit3/filesystem/FilesystemException.php');
+require_once(__DIR__ . '/../src/bit3/filesystem/Util.php');
+
+require_once(__DIR__ . '/../src/bit3/filesystem/iterator/FilesystemIterator.php');
+require_once(__DIR__ . '/../src/bit3/filesystem/iterator/RecursiveFilesystemIterator.php');
+
+require_once(__DIR__ . '/../src/bit3/filesystem/local/LocalFile.php');
+require_once(__DIR__ . '/../src/bit3/filesystem/local/LocalFilesystem.php');
+
+require_once(__DIR__ . '/../src/bit3/filesystem/merged/MergedFile.php');
+require_once(__DIR__ . '/../src/bit3/filesystem/merged/MergedFilesystem.php');
+require_once(__DIR__ . '/../src/bit3/filesystem/merged/VirtualFile.php');

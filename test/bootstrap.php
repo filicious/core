784bde617fd740caeba9c223bce8292616073822
Tristan Lins <tristan.lins@infinitysoft.de>
2012-10-25 21:47:09 +0200

    Add autoloader to phpunit bootstrap file.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -9,17 +9,9 @@
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
 
-require_once(__DIR__ . '/../src/bit3/filesystem/File.php');
-require_once(__DIR__ . '/../src/bit3/filesystem/Filesystem.php');
-require_once(__DIR__ . '/../src/bit3/filesystem/FilesystemException.php');
-require_once(__DIR__ . '/../src/bit3/filesystem/Util.php');
-
-require_once(__DIR__ . '/../src/bit3/filesystem/iterator/FilesystemIterator.php');
-require_once(__DIR__ . '/../src/bit3/filesystem/iterator/RecursiveFilesystemIterator.php');
-
-require_once(__DIR__ . '/../src/bit3/filesystem/local/LocalFile.php');
-require_once(__DIR__ . '/../src/bit3/filesystem/local/LocalFilesystem.php');
-
-require_once(__DIR__ . '/../src/bit3/filesystem/merged/MergedFile.php');
-require_once(__DIR__ . '/../src/bit3/filesystem/merged/MergedFilesystem.php');
-require_once(__DIR__ . '/../src/bit3/filesystem/merged/VirtualFile.php');
+spl_autoload_register(function($class) {
+    $path = __DIR__ . '/../src/' . implode('/', explode('\\', $class)) . '.php';
+    if (file_exists($path)) {
+        include($path);
+    }
+});

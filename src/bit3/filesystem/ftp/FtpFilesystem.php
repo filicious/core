0ca8380c2326cfc425d73f2bb35aba65208ec2c3
Christian Schiffler <c.schiffler@cyberspectrum.de>
2012-12-05 22:55:20 +0100

    Fixed regex for FTP filesystem file list parsing. Day numbers in date section may consist of either 1 or 2 digits (was broken as we have no "zero" as prefix for, at least, proftpd).

--- src/bit3/filesystem/ftp/FtpFilesystem.php
+++ src/bit3/filesystem/ftp/FtpFilesystem.php
@@ -188,7 +188,7 @@ class FtpFilesystem
             $isSingleFile = true;
 
             foreach ($list as $item) {
-                if (preg_match('#^([\-ldrwxsSt]{10})\s+(\d+)\s+([\w\d]+)\s+([\w\d]+)\s+(\d+)\s+(\w{3}\s+\d{2}\s+(?:\d{2}:\d{2}|\d{4}))\s+(.*?)(\s+->\s+(.*))?$#s', $item, $match)) {
+                if (preg_match('#^([\-ldrwxsSt]{10})\s+(\d+)\s+([\w\d]+)\s+([\w\d]+)\s+(\d+)\s+(\w{3}\s+\d{1,2}\s+(?:\d{2}:\d{2}|\d{4}))\s+(.*?)(\s+->\s+(.*))?$#s', $item, $match)) {
                     $stat = (object) array(
                         'perms'       => $match[1],
                         'mode'        => Util::string2bitMode($match[1]),

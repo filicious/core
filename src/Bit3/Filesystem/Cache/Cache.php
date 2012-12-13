4bed86fbc4d1712c935f80495db08df1afd5e351
Tristan Lins <tristan.lins@bit3.de>
2012-12-13 19:24:44 +0100

    Improve Cache interface, add Cache::exists and Cache::delete and $ttl.

--- src/Bit3/Filesystem/Cache/Cache.php
+++ src/Bit3/Filesystem/Cache/Cache.php
@@ -20,17 +20,44 @@ namespace Bit3\Filesystem\Cache;
 interface Cache
 {
 	/**
-	 * @param string $key
+	 * Check if key exists in the data source.
+	 *
+	 * @param string $key The key used to store the value.
+	 *
+	 * @return bool
+	 */
+	public function exists($key);
+
+	/**
+	 * Fetch a stored variable from the cache.
+	 *
+	 * @param string $key The key used to store the value.
 	 *
 	 * @return mixed
 	 */
 	public function fetch($key);
 
 	/**
-	 * @param string $key
-	 * @param mixed  $value
+	 * Removes a stored variable from the cache.
 	 *
-	 * @return mixed
+	 * @param string $key The key used to store the value.
+	 *
+	 * @return bool
 	 */
-	public function store($key, $value);
+	public function delete($key);
+
+	/**
+	 * Cache a variable in the data store.
+	 *
+	 * @param string $key   Store the variable using this name. Keys are cache-unique,
+	 *                      so storing a second value with the same key will overwrite the original value.
+	 * @param mixed  $value The variable to store.
+	 * @param int    $ttl   Time in seconds Time To Live; store var in the cache for ttl seconds.
+	 *                      After the ttl has passed, the stored variable will be expunged from the cache.
+	 *                      If no ttl is supplied (or if the ttl is 0),
+	 *                      the value will persist until it is removed from the cache manually.
+	 *
+	 * @return bool
+	 */
+	public function store($key, $value, $ttl = 0);
 }

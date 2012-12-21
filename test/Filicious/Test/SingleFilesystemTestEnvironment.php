8ae416fdbd529d0e78c0904f6f8284c042c0e04b
Tristan Lins <tristan.lins@bit3.de>
2012-12-21 13:43:58 +0100

    Rework test case structure.

---
+++ test/Filicious/Test/SingleFilesystemTestEnvironment.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * High level object oriented filesystem abstraction.
+ *
+ * @package filicious-core
+ * @author  Tristan Lins <tristan.lins@bit3.de>
+ * @author  Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author  Oliver Hoff <oliver@hofff.com>
+ * @link    http://filicious.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+namespace Filicious\Test;
+
+require_once(__DIR__ . '/../../bootstrap.php');
+
+/**
+ * A test adapter allow direct access to a filesystem.
+ * This adapter is an alternative implementation to a filesystem.
+ */
+interface SingleFilesystemTestEnvironment
+{
+	/**
+	 * @return TestAdapter
+	 */
+	public function getAdapter();
+
+	/**
+	 * @return \Filicious\Filesystem
+	 */
+	public function getFilesystem();
+
+	/**
+	 * Cleanup environment.
+	 */
+	public function cleanup();
+}

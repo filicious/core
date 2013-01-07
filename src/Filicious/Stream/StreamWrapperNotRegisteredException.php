eb54ec22b757766f720d136a89c8b25402a99cca
Tristan Lins <tristan.lins@bit3.de>
2013-01-07 01:25:13 +0100

    Add missing class StreamWrapperNotRegisteredException.

---
+++ src/Filicious/Stream/StreamWrapperNotRegisteredException.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * High level object oriented filesystem abstraction.
+ *
+ * @package filicious-core
+ * @author  Tristan Lins <tristan.lins@bit3.de>
+ * @author  Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author  Oliver Hoff <oliver@hofff.com>
+ * @link    http://filicious.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+namespace Filicious\Stream;
+
+/**
+ * Stream wrapper already registered exception.
+ *
+ * @package filicious-core
+ * @author  Tristan Lins <tristan.lins@bit3.de>
+ */
+class StreamWrapperNotRegisteredException extends StreamException
+{
+	protected $scheme;
+
+	protected $host;
+
+	public function __construct($scheme, $host = false)
+	{
+		$this->scheme = $scheme;
+		$this->host   = $host;
+		parent::__construct(
+			sprintf(
+				'There is no stream wrapper registered for scheme %s://%s!',
+				$scheme,
+				$host
+			)
+		);
+	}
+
+	public function getScheme()
+	{
+		return $this->scheme;
+	}
+
+	public function getHost()
+	{
+		return $this->host;
+	}
+}
